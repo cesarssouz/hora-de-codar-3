962033d0360aead86c0d77f1a166fc0d4b1b976d
César Souza <cesarsouz10@gmail.com>
2024-09-18 16:42:43 -0300

    Add files via upload

---
+++ Exercicio 3 js/js/ex4.js
@@ -0,0 +1,13 @@
+var inicio = 15
+var fim = 100
+var soma = 0
+var quantidade = 0
+
+while(inicio <=fim){
+    soma += inicio
+    inicio++
+    quantidade++
+
+}
+var media = soma/ quantidade
+document.write(media)
